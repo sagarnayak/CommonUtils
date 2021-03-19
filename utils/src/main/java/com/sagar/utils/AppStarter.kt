44fbf1af34877d7d68f88ef31d4a9d7c16326239
sagar <sagar@oneolabs.com>
2021-03-19 16:11:45 +0530

    added app starter

---
+++ utils/src/main/java/com/sagar/utils/AppStarter.kt
@@ -0,0 +1,21 @@
+package com.sagar.utils
+
+import android.content.Context
+import android.content.Intent
+import com.sagar.modelsandenums.KeywordsAndConstants.END_SELF
+
+class AppStarter(private val context: Context) {
+
+    fun <T> restartApp(targetSplashActivity: Class<T>) {
+        context.startActivity(
+            Intent(
+                context,
+                targetSplashActivity
+            )
+                .setFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK
+                )
+        )
+        context.sendBroadcast(Intent(END_SELF))
+    }
+}
