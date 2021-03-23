51df08b144ce057a0444c066536ef89d3ae20a42
sagar <sagar@oneolabs.com>
2021-03-23 11:02:03 +0530

    update

---
+++ utils/src/main/java/com/sagar/utils/ConversionUtil.kt
@@ -0,0 +1,29 @@
+package com.sagar.utils
+
+import android.content.Context
+import android.util.DisplayMetrics
+import kotlin.math.roundToInt
+
+
+/**
+ * class to make various conversions.
+ */
+@Suppress("unused")
+object ConversionUtil {
+    /**
+     * to convert dp to px
+     *
+     * @param dp      your required dp
+     * @param context context
+     * @return the result px value
+     */
+    fun dpToPx(dp: Int, context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return (dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
+    }
+
+    fun bytesToMB(length: Long): Long {
+        val kb = length / 1024
+        return kb / 1024
+    }
+}
